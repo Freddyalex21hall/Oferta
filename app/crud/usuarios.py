9fb6506298fb6ec4cced9d8190ab1b22c55c0b3f
miguel angel pardo <miguel.pardo@utp.edu.co>
2025-11-10 22:26:08 -1200

    ajustes

--- app/crud/usuarios.py
+++ app/crud/usuarios.py
@@ -1,4 +1,3 @@
-from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
@@ -12,14 +11,14 @@ logger = logging.getLogger(__name__)
 
 def create_user(db: Session, user: CrearUsuario) -> Optional[bool]:
     try:
-        dataUser = user.model_dump() # convierte el esquema en diccionario
-        contraOrigin = dataUser["contra_encript"] # saca la contra original
-        contraEncript = get_hashed_password(contraOrigin) # envia la contra original a encriptar 
-        dataUser["contra_encript"] = contraEncript # remplaza la contra original por la encriptada
+        dataUser = user.model_dump() # convierte el schema en diccionario
+        contra_original = dataUser["contra_encript"] # saca la contra original
+        contra_encript = get_hashed_password(contra_original) # encripta la contraseña y la guarda en la variable
+        dataUser["contra_encript"] = contra_encript
 
         query = text("""
             INSERT INTO usuario (
-                nombre_completo, num_documento, 
+                nombre_completo, num_documento,
                 correo, contra_encript, id_rol,
                 estado
             ) VALUES (
@@ -30,92 +29,89 @@ def create_user(db: Session, user: CrearUsuario) -> Optional[bool]:
         """)
         db.execute(query, dataUser)
         db.commit()
-
         return True
     except Exception as e:
         db.rollback()
         logger.error(f"Error al crear usuario: {e}")
         raise Exception("Error de base de datos al crear el usuario")
-
+    
 def get_user_by_id(db: Session, id_usuario:int):
     try:
         query = text("""
-            SELECT usuario.id_usuario, usuario.nombre_completo, 
-                   usuario.num_documento, usuario.correo, usuario.id_rol, 
-                   usuario.estado, rol.nombre_rol
-            FROM usuario
-            INNER JOIN rol ON usuario.id_rol = rol.id_rol
-            WHERE usuario.id_usuario = :id_user
+                SELECT usuario.id_usuario, usuario.nombre_completo, usuario.num_documento, usuario.correo, usuario.id_rol, usuario.estado, rol.nombre_rol
+                FROM usuario
+                INNER JOIN rol ON usuario.id_rol = rol.id_rol
+                WHERE usuario.id_usuario = :id_user
         """)
 
         result = db.execute(query, {"id_user": id_usuario}).mappings().first()
         return result
     
     except SQLAlchemyError as e:
-        logger.error(f"Error al bucar usuario por id: {e}")
+        db.rollback()
+        logger.error(f"Error al buscar usuario por id: {e}")
         raise Exception("Error de base de datos al buscar el usuario")
-
-
+    
 def get_user_by_email(db: Session, un_correo:str):
     try:
         query = text("""
-            SELECT usuario.id_usuario, usuario.nombre_completo, 
-                usuario.num_documento, usuario.correo, usuario.id_rol, 
-                usuario.estado, rol.nombre_rol
-            FROM usuario
-            INNER JOIN rol ON usuario.id_rol = rol.id_rol
-            WHERE usuario.correo = :email
+                SELECT usuario.id_usuario, usuario.nombre_completo, usuario.num_documento, usuario.correo, usuario.id_rol, usuario.estado, rol.nombre_rol
+                FROM usuario
+                INNER JOIN rol ON usuario.id_rol = rol.id_rol
+                WHERE usuario.correo = :email
         """)
 
         result = db.execute(query, {"email": un_correo}).mappings().first()
         return result
     
     except SQLAlchemyError as e:
-        logger.error(f"Error al bucar usuario por email: {e}")
+        db.rollback()
+        logger.error(f"Error al buscar usuario por email: {e}")
         raise Exception("Error de base de datos al buscar el usuario por correo")
+    
 
 def get_user_by_email_security(db: Session, un_correo:str):
     try:
         query = text("""
-            SELECT usuario.id_usuario, usuario.nombre_completo, 
-            usuario.num_documento, usuario.correo, usuario.contra_encript, usuario.id_rol, 
-            usuario.estado, rol.nombre_rol
-            FROM usuario
-            INNER JOIN rol ON usuario.id_rol = rol.id_rol
-            WHERE usuario.correo = :email
+                SELECT usuario.id_usuario, usuario.nombre_completo, usuario.contra_encript, usuario.num_documento, usuario.correo, usuario.id_rol, usuario.estado, rol.nombre_rol
+                FROM usuario
+                INNER JOIN rol ON usuario.id_rol = rol.id_rol
+                WHERE usuario.correo = :email
         """)
 
         result = db.execute(query, {"email": un_correo}).mappings().first()
         return result
     
     except SQLAlchemyError as e:
-        logger.error(f"Error al bucar usuario por email: {e}")
+        db.rollback()
+        logger.error(f"Error al buscar usuario por email: {e}")
         raise Exception("Error de base de datos al buscar el usuario por correo")
-
+    
+    
 def user_delete(db: Session, id:int):
     try:
         query = text("""
-            DELETE FROM usuario
-            WHERE usuario.id_usuario = :el_id
+                DELETE
+                FROM usuario
+                WHERE usuario.id_usuario = :el_id
         """)
 
         db.execute(query, {"el_id": id})
         db.commit()
-        
         return True
     
     except SQLAlchemyError as e:
         db.rollback()
         logger.error(f"Error al eliminar usuario por id: {e}")
-        raise Exception("Error de base de datos al eliminar el usuario")
-
-
+        raise Exception("Error de base de datos al eliminar el usuario por id")
+    
+    
 def update_user(db: Session, user_id: int, user_update: EditarUsuario) -> bool:
     try:
-        fields = user_update.model_dump(exclude_unset=True)
+        fields = user_update.model_dump(exclude_unset=True) # exclude_unset=True si no llegan todos los datos los que no los excluye para evitar null 
         if not fields:
             return False
-        set_clause = ", ".join([f"{key} = :{key}" for key in fields])
+        set_clause = ", ".join([f"{key} = :{key}" for key in fields]) # recibe la informacion y guarda la clave y el parametro de consulta / .join no modifica el valor, lo agrega, concatena  
         fields["user_id"] = user_id
 
         query = text(f"UPDATE usuario SET {set_clause} WHERE id_usuario = :user_id")
@@ -128,15 +124,12 @@ def update_user(db: Session, user_id: int, user_update: EditarUsuario) -> bool:
         raise Exception("Error de base de datos al actualizar el usuario")
 
 
-
 def update_password(db: Session, user_data: EditarPass) -> bool:
     try:
-        datos_usuario = user_data.model_dump()
-        contra_encript = get_hashed_password(datos_usuario['contra_nueva'])
+        datos_usuario = user_data.model_dump(exclude_unset=True)
+        contra_encript= get_hashed_password(datos_usuario['contra_nueva'])
         datos_usuario['pass_encript'] = contra_encript
-
-        query = text(f""" UPDATE usuario SET contra_encript = :pass_encript 
-                        WHERE id_usuario = :id_usuario """)
+        query = text(f"UPDATE usuario SET contra_encript= :pass_encript WHERE id_usuario = :id_usuario")
         db.execute(query, datos_usuario)
         db.commit()
         return True
@@ -144,29 +137,25 @@ def update_password(db: Session, user_data: EditarPass) -> bool:
         db.rollback()
         logger.error(f"Error al actualizar usuario: {e}")
         raise Exception("Error de base de datos al actualizar el usuario")
-
-
-def verify_user_pass(db: Session, user_data: EditarPass) -> bool:
+    
+def verify_user_pass(db: Session, user_data:EditarPass):
     try:
         query = text("""
-            SELECT usuario.contra_encript
-            FROM usuario
-            WHERE usuario.id_usuario = :id_user
+                SELECT usuario.contra_encript
+                FROM usuario
+                WHERE usuario.id_usuario = :id_user
         """)
 
-        result = db.execute(query, {"id_user": user_data.id_usuario }).mappings().first()
-        contra_en_db = result.contra_encript
-        contra_anterior = user_data.contra_anterior
-        print(contra_en_db)
-        print(contra_anterior)
+        result = db.execute(query, {"id_user": user_data.id_usuario}).mappings().first()
+        contra_db= result.contra_encript
+        contra_anterior= user_data.contra_anterior
 
-        validated = verify_password(contra_anterior, contra_en_db)
-
+        validated = verify_password(contra_anterior,contra_db)
         if not validated:
             return False
         else:
             return True
     
     except SQLAlchemyError as e:
-        logger.error(f"Error al bucar validar la contraseña: {e}")
-        raise Exception("Error de base de datos al validar la contraseña")
+        logger.error(f"Error al validar la contraseña: {e}")
+        raise Exception("Error de base de datos validar lacontraseña")
