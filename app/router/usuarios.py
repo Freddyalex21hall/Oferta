516a9b84aeb7479593062394fd111a302d629725
freddyalex21hall <kevinalextor@gmail.com>
2025-11-19 12:10:26 -0500

    Versionando

--- app/router/usuarios.py
+++ app/router/usuarios.py
@@ -62,6 +62,31 @@ def get_by_email(
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@router.get("/obtener-todos}", status_code=status.HTTP_200_OK, response_model=List[RetornoUsuario])
+def get_all(db: Session = Depends(get_db)):
+    try:
+        users = crud_users.get_all_user(db)
+        if users is None:
+            raise HTTPException(status_code=404, detail="Usuarios no encontrados")
+        return users
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/obtener-todos-secure", status_code=status.HTTP_200_OK, response_model=List[RetornoUsuario])
+def get_all_s(
+    db: Session = Depends(get_db),
+    user_token: RetornoUsuario = Depends(get_current_user)
+):
+    try:
+        if user_token.id_rol != 1:
+            raise HTTPException(status_code=401, detail="No tienes permisos para crear usuario")
+        
+        users = crud_users.get_all_user(db)
+        if users is None:
+            raise HTTPException(status_code=404, detail="Usuarios no encontrados")
+        return users
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/eliminar-por-id/{id_usuario}", status_code=status.HTTP_200_OK)
 def delete_by_id(
@@ -114,31 +139,5 @@ def update_password(
         if not success:
             raise HTTPException(status_code=400, detail="No se pudo actualizar la contraseña del usuario")
         return {"message": "Contraseña actualizada correctamente"}
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@router.get("/obtener-todos}", status_code=status.HTTP_200_OK, response_model=List[RetornoUsuario])
-def get_all(db: Session = Depends(get_db)):
-    try:
-        users = crud_users.get_all_user(db)
-        if users is None:
-            raise HTTPException(status_code=404, detail="Usuarios no encontrados")
-        return users
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@router.get("/obtener-todos-secure", status_code=status.HTTP_200_OK, response_model=List[RetornoUsuario])
-def get_all_s(
-    db: Session = Depends(get_db),
-    user_token: RetornoUsuario = Depends(get_current_user)
-):
-    try:
-        if user_token.id_rol != 1:
-            raise HTTPException(status_code=401, detail="No tienes permisos para crear usuario")
-        
-        users = crud_users.get_all_user(db)
-        if users is None:
-            raise HTTPException(status_code=404, detail="Usuarios no encontrados")
-        return users
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
