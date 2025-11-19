a00fc16ac45f58c44f4ec0c65a3ee2d37436b422
freddyalex21hall <kevinalextor@gmail.com>
2025-11-19 10:35:30 -0500

    Versionando_H

--- mi_db.sql
+++ mi_db.sql
@@ -88,3 +88,23 @@ CREATE TABLE IF NOT EXISTS `grupos` (
     FOREIGN KEY(`cod_estrategia`) REFERENCES `estrategia`(`cod_estrategia`)
     ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+CREATE TABLE historico(
+    id_historico INT AUTO_INCREMENT PRIMARY KEY,
+    id_grupo INTEGER UNSIGNED NOT NULL,  -- Cambiado de INT a INTEGER UNSIGNED
+    num_aprendices_inscritos SMALLINT,
+    num_aprendices_en_transito SMALLINT,
+    num_aprendices_formacion SMALLINT,
+    num_aprendices_induccion SMALLINT,
+    num_aprendices_condicionados SMALLINT,
+    num_aprendices_aplazados SMALLINT,
+    num_aprendices_retirado_voluntario SMALLINT,
+    num_aprendices_cancelados SMALLINT,
+    num_aprendices_reprobados SMALLINT,
+    num_aprendices_no_aptos SMALLINT,
+    num_aprendices_reingresados SMALLINT,
+    num_aprendices_por_certificar SMALLINT,
+    num_aprendices_certificados SMALLINT,
+    num_aprendices_trasladados SMALLINT,
+    FOREIGN KEY (id_grupo) REFERENCES grupos(ficha)
+);
